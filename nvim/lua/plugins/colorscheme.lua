d6c0c0fa35d015ecaad581d2858cdc89c9407111
Badal <badalWork1@gmail.com>
2023-12-15 15:50:04 +0530

    fix: fix some colors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,59 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			transparent = true,
-			terminal_colors = true,
-			styles = {
+	-- "folke/tokyonight.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	-- 	require("tokyonight").setup({
+	-- 		style = "storm",
+	-- 		transparent = true,
+	-- 		terminal_colors = true,
+	-- 		styles = {
+	-- 			sidebars = "transparent",
+	-- 			floats = "transparent",
+	-- 		},
+	-- 		on_colors = function(colors)
+	-- 			colors.comment = "#5c6370"
+	-- 		end,
+	-- 		on_highlights = function(highlights, colors)
+	-- 			highlights.CursorLineNr = {
+	-- 				bold = true,
+	-- 			}
+	-- 		end,
+	-- 	})
+	--
+	-- 	vim.cmd("colorscheme tokyonight")
+	-- end,
 
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-			on_colors = function(colors)
-				colors.comment = "#5c6370"
-			end,
-			on_highlights = function(highlights, colors)
-				highlights.CursorLineNr = {
-					bold = true,
-				}
-			end,
-		})
+	--
 
-		vim.cmd("colorscheme tokyonight")
-	end,
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				transparent_background = true,
+				show_end_of_buffer = false,
+
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					telescope = true,
+					notify = true,
+					mini = true,
+				},
+				color_overrides = {
+					mocha = {
+						base = "#000000",
+						mantle = "#000000",
+						crust = "#000000",
+					},
+				},
+			})
+			vim.cmd("colorscheme catppuccin")
+		end,
+	},
 }
