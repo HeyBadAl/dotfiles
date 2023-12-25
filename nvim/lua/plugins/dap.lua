8860da8c3c11588b1239ef54c3646b67a8e6bb17
Badal <badalWork1@gmail.com>
2023-12-25 12:33:59 +0530

    feat: added go lsp, dap
    - added some plugins for wrinting go projects

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,21 @@
+return {
+
+	{
+		"mfussenegger/nvim-dap",
+		optional = true,
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					ensure_installed = {
+						"delve",
+					},
+				},
+			},
+			{
+				"leoluz/nvim-dap-go",
+				config = true,
+			},
+		},
+	},
+}
