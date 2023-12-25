26adeba2a4166d8eede113bce16e20e4db2839be
Badal <badalWork1@gmail.com>
2023-12-25 20:35:45 +0530

    chore: added some keymaps
    - added virtual-text for debuggin and added some keybindings
    - also configure some default setup for nvim-dap-ui

--- nvim/lua/plugins/go-setup.lua
+++ nvim/lua/plugins/go-setup.lua
@@ -1,126 +1,59 @@
 return {
+
+	-- nvim-dap
 	{
-		"ray-x/go.nvim",
-		dependencies = {
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			require("go").setup()
-		end,
-		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()',
-		event = { "CmdlineEnter" },
-		keys = {
-			{ "<leader>gd", "<cmd>GoDef<cr>", desc = "Go to definition" },
-			{ "<leader>gi", "<cmd>GoImpl<cr>", desc = "Go to implementation" },
-			{ "<leader>gt", "<cmd>GoTest<cr>", desc = "Go to test" },
-			{ "<leader>gr", "<cmd>GoReferrers<cr>", desc = "Go to referrers" },
-			{ "<leader>gc", "<cmd>GoCallers<cr>", desc = "Go to callers" },
-			{ "<leader>gb", "<cmd>GoBuild<cr>", desc = "Go build" },
-			{ "<leader>gt", "<cmd>GoTestFunc<cr>", desc = "Go test function" },
-			{ "<leader>ge", "<cmd>GoIfErr<cr>", desc = "Go if error" },
-			{ "<leader>gs", "<cmd>GoFillStruct<cr>", desc = "Go fill struct" },
-			{ "<leader>ga", "<cmd>GoAlternate<cr>", desc = "Go alternate" },
-			{ "<leader>gA", "<cmd>GoAddTags<cr>", desc = "Go add tags" },
-			{ "<leader>gR", "<cmd>GoRemoveTags<cr>", desc = "Go remove tags" },
-			{ "<leader>gr", "<cmd>GoRename<cr>", desc = "Go rename" },
-			{ "<leader>gS", "<cmd>GoFillStruct<cr>", desc = "Go fill struct" },
-			{ "<leader>gC", "<cmd>GoCmt<cr>", desc = "Go comment" },
-      -- go dap ui 
-      { "<leader>gD", "<cmd>lua require('dap-go').debug_test()<cr>", desc = "Go debug test" },
-      { "<leader>gd", "<cmd>lua require('dap-go').debug_test_package()<cr>", desc = "Go debug test package" },
-		},
-
-		opts = {
-			goimport = "gopls",
-			gofmt = "gopls",
-			max_line_len = 120,
-			comment_placeholder = "   ",
-			icons = { breakpoint = "🧘", currentpos = "🏃" },
-			tag_transform = "snakecase",
-			diagnostic = {
-				hdlr = false, -- hook lsp diag handler and send diag to quickfix
-				underline = true,
-				-- virtual text setup
-				virtual_text = { spacing = 0, prefix = "■" },
-				signs = true,
-				update_in_insert = false,
-			},
-
-			dap_debug = true,
-			dap_deug_gui = true,
-			dap_debug_gui = true,
-			dap_debug_keymap = true,
-
-			dap_vt = true,
-			test_runner = "go",
-			run_in_floaterm = true,
-		},
+		"mfussenegger/nvim-dap",
 	},
 
 	-- nvim-dap-go
 	{
 		"leoluz/nvim-dap-go",
-		config = true,
-		optional = true,
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-telescope/telescope-dap.nvim",
-			"theHamsta/nvim-dap-virtual-text",
-			"rcarriga/nvim-dap-ui",
-			"jbyuki/one-small-step-for-vimkind",
-		},
-		ft = { "go", "gomod" },
 	},
 
-	-- neotest-go
+	-- nvim-dap-ui
 	{
-		"nvim-neotest/neotest-go",
-		optional = true,
-		dependencies = {
-			"nvim-neotest/neotest-go",
-		},
-		opts = {
-			adapters = {
-				["neotest-go"] = {},
-			},
-		},
-	},
-
-	-- none-ls.nvim
-	{
-		"nvimtools/none-ls.nvim",
-		optional = true,
-		dependencies = {
-			{
-				"williamboman/mason.nvim",
-				opts = function(_, opts)
-					opts.ensure_installed = opts.ensure_installed or {}
-					vim.list_extend(opts.ensure_installed, { "gomodifytags", "impl" })
-				end,
-			},
-		},
-		opts = function(_, opts)
-			local nls = require("null-ls")
-			opts.sources = vim.list_extend(opts.sources or {}, {
-				nls.builtins.code_actions.gomodifytags,
-				nls.builtins.code_actions.impl,
-				nls.builtins.formatting.goimports,
-				nls.builtins.formatting.gofumpt,
+		"rcarriga/nvim-dap-ui",
+		config = function()
+			require("dapui").setup({
+				icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+				mappings = {
+					-- Use a table to apply multiple mappings
+					expand = { "<CR>", "<2-LeftMouse>" },
+					open = "o",
+					remove = "d",
+					edit = "e",
+					repl = "r",
+					toggle = "t",
+				},
 			})
 		end,
 	},
 
-	-- conform.nvim
+	-- nvim-virtual-text-go
 	{
-		"stevearc/conform.nvim",
-		optional = true,
-		opts = {
-			formatters_by_ft = {
-				go = { "goimports", "gofumpt" },
-			},
+		"theHamsta/nvim-dap-virtual-text",
+		config = function()
+			require("nvim-dap-virtual-text").setup({
+				enabled = true,
+				enabled_commands = true,
+				highlight_changed_variables = true,
+				highlight_new_as_changed = true,
+				show_stop_reason = true,
+				commented = false,
+				only_first_definition = true,
+				all_references = false,
+				clear_on_continue = false,
+			})
+		end,
+
+		-- keymaps
+		keys = {
+			{ "<leader>dt", "<cmd>lua require('dap-go').debug_test()<cr>", desc = "Debug Test" },
+			{ "<leader>db", "<cmd>lua require('dap-go').debug_breakpoint()<cr>", desc = "Debug Breakpoint" },
+			{ "<leader>ds", "<cmd>lua require('dap-go').debug_stop()<cr>", desc = "Debug Stop" },
+			{ "<leader>dr", "<cmd>lua require('dap-go').debug_restart()<cr>", desc = "Debug Restart" },
+			{ "<leader>di", "<cmd>lua require('dap-go').debug_info()<cr>", desc = "Debug Info" },
+			{ "<leader>dl", "<cmd>lua require('dap-go').debug_last()<cr>", desc = "Debug Last" },
 		},
 	},
 }
