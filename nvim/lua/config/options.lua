45cae03166050f079568448ba24d027184a05b8a
Badal <badalWork1@gmail.com>
2024-01-01 20:06:07 +0530

    Chore: Refactor Neovim configuration files for better organization and add new features
    - **options.lua**: Improve and organize global options, set autowrite, conceallevel, confirm, cursorline, formatoptions, grepformat, grepprg, ignorecase, inccommand, laststatus, list, pumblend, pumheight, scrolloff, sessionoptions, shiftround, shiftwidth, shortmess, showmode, sidescrolloff, spelllang, splitbelow, splitright, timeoutlen, undofile, updatetime, writebackup, winminwidth, fillchars, smoothscroll (if supported), and markdown_recommended_style.
    
    - **globals.lua**: Set mapleader, maplocalleader, and introduce autoformat global variable.
    
    These changes aim to enhance Neovim's configuration, improve readability, and introduce new features for a smoother editing experience.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -46,3 +46,49 @@ opt.guicursor =
 	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 opt.encoding = "UTF-8"
 opt.showmode = false
+
+-- idk
+opt.autowrite = true
+opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true -- Highlight current line
+-- opt.expandtab = true -- Use spaces instead of tabs
+opt.formatoptions = "jcroqlnt" -- tcqj
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep" -- Use rg as grep program
+opt.ignorecase = true -- Ignore case
+opt.inccommand = "nosplit" -- Get substitute preview
+opt.laststatus = 3 -- Global statusline
+opt.list = true -- Show some invisible characters (tabs...
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.scrolloff = 4 -- Lines of context
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
+opt.shiftround = true -- Round indent
+opt.shiftwidth = 2 -- Size of an indent
+opt.shortmess:append({ W = true, I = true, c = true })
+opt.showmode = false -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8 -- Columns of context
+opt.spelllang = { "en" }
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true -- Put new windows right of current
+opt.timeoutlen = 300
+opt.undofile = true
+opt.updatetime = 250
+opt.writebackup = false
+opt.winminwidth = 5
+
+opt.fillchars = {
+	foldopen= "",
+	foldclose= "",
+	eob = " ",
+	diff = "╱",
+	fold = " ",
+	foldsep = " ",
+}
+
+if vim.fn.has("nvim-0.10") == 1 then
+	opt.smoothscroll = true
+end
+
+vim.g.markdown_recommended_style = 0
