b289309ae7aabd2d506e93e387d6068c8d9626ad
Badal <badalWork1@gmail.com>
2024-01-01 21:37:34 +0530

    Refactor: Streamline Neovim configuration for Mason debugger in 'mason.lua'
    - Remove redundant and commented-out options.
    - Simplify plugin installation and setup using default configurations from 'mason.nvim'.
    - Ensure compatibility with DAP by triggering the setup on 'BufReadPre'.
    
    These changes enhance clarity and conciseness in the Mason debugger configuration.

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -21,16 +21,8 @@ return {
 		dependencies = "mason.nvim",
 		cmd = { "DapInstall", "DapUninstall" },
 		opts = {
-			-- Makes a best effort to setup the various debuggers with
-			-- reasonable debug configurations
 			automatic_installation = true,
-
-			-- You can provide additional configuration to the handlers,
-			-- see mason-nvim-dap README for more information
 			handlers = {},
-
-			-- You'll need to check that you have the required things installed
-			-- online, please don't ask me how to install them :)
 			ensure_installed = {
 				-- Update this to ensure that you have the debuggers for the langs you want
 			},
@@ -59,6 +51,7 @@ return {
 			},
 		},
 		event = "BufReadPre",
+
 		dependencies = "williamboman/mason.nvim",
 	},
 }
