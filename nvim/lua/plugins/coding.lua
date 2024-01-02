839f777235ff6a59322810b615d28fd69ba20ee6
Badal <badalWork1@gmail.com>
2024-01-02 12:36:41 +0530

    feat: added new version of nvim config using lazyvim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,288 +1,184 @@
 return {
-	-- cmp-nvim-lsp
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
-	{ "hrsh7th/cmp-cmdline" },
-	{ "hrsh7th/cmp-emoji" },
+  { import = "plugins.extras.coding.codeium" },
 
-	-- friendly snippets
-	{
-		"rafamadriz/friendly-snippets",
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
+  -- comment
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
+  },
 
-	-- luasnip
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-		opts = {
-			history = true,
-			delete_check_events = "TextChanged",
-		},
-		keys = {
-			{
-				"<tab>",
-				function()
-					return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-				end,
-				expr = true,
-				silent = true,
-				mode = "i",
-			},
-			{
-				"<tab>",
-				function()
-					require("luasnip").jump(1)
-				end,
-				mode = "s",
-			},
-			{
-				"<s-tab>",
-				function()
-					require("luasnip").jump(-1)
-				end,
-				mode = { "i", "s" },
-			},
-		},
-	},
+  -- luaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    build = (not jit.os:find("Windows"))
+        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+      or nil,
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+  -- stylua: ignore
+  keys = {
+    {
+      "<tab>",
+      function()
+        return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+      end,
+      expr = true, silent = true, mode = "i",
+    },
+    { "<C-j>", function() require("luasnip").jump(1) end, mode = "s" },
+    { "<C-k>", function() require("luasnip").jump(-1) end, mode = "s" },
+  },
+  },
 
-	-- nvim-cmp
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			-- codeium
-			{
-				"Exafunction/codeium.nvim",
-				cmd = "Codeium",
-				build = ":Codeium Auth",
-				opts = {},
-			},
-		},
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+  -- cmp-nvim-lsp
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-emoji",
+    "saadparwaiz1/cmp_luasnip",
+  },
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-k>"] = cmp.mapping.select_prev_item(),
-					["<C-j>"] = cmp.mapping.select_next_item(),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<S-CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}),
-					["<C-CR>"] = function(fallback)
-						cmp.abort()
-						fallback()
-					end,
-				}),
-				sources = cmp.config.sources({
-					{ name = "luasnip" }, -- For luasnip users.
-					{ name = "nvim_lsp" },
-					{ name = "path" },
-					{ name = "cmdline" },
-					{ name = "emoji" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+  -- nvim-cmp
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    opts = function()
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "cmdline" },
+          { name = "emoji" },
+        }, {
+          { name = "buffer" },
+        }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("lazyvim.config").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+    ---@param opts cmp.ConfigSchema
+    config = function(_, opts)
+      for _, source in ipairs(opts.sources) do
+        source.group_index = source.group_index or 1
+      end
+      require("cmp").setup(opts)
+    end,
+  },
 
-	-- mini-pairs
-	{
-		"echasnovski/mini.pairs",
-		opts = {},
-		keys = {
-			{
-				"<leader>up",
-				function()
-					local Util = require("lazy.core.util")
-					vim.g.minipairs_disable = not vim.g.minipairs_disable
-					if vim.g.minipairs_disable then
-						Util.warn("Disabled auto pairs", { title = "Option" })
-					else
-						Util.info("Enabled auto pairs", { title = "Option" })
-					end
-				end,
-				desc = "Toggle auto pairs",
-			},
-		},
-	},
+  -- autopairs
+  {
+    "windwp/nvim-autopairs",
+    event = { "InsertEnter" },
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      -- import nvim-autopairs
+      local autopairs = require("nvim-autopairs")
 
-	-- mini-sorround
-	{
-		"echasnovski/mini.surround",
-		config = function()
-			require("mini.surround").setup({
-				mappings = {
-					add = "sa", -- Add surrounding in Normal and Visual modes
-					delete = "sd", -- Delete surrounding
-					find = "sf", -- Find surrounding (to the right)
-					find_left = "sF", -- Find surrounding (to the left)
-					highlight = "sh", -- Highlight surrounding
-					replace = "sr", -- Replace surrounding
-					update_n_lines = "sn", -- Update `n_lines`
+      -- configure autopairs
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+        ts_config = {
+          lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+          javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+          java = false, -- don't check treesitter on java
+        },
+      })
 
-					suffix_last = "l", -- Suffix to search with "prev" method
-					suffix_next = "n", -- Suffix to search with "next" method
-				},
-			})
-		end,
-	},
+      -- import nvim-autopairs completion functionality
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-	-- comment
-	{
-		"numToStr/Comment.nvim",
-		opts = {
-			-- add any options here
-		},
-		lazy = false,
-	},
+      -- import nvim-cmp plugin (completions plugin)
+      local cmp = require("cmp")
 
-	-- codeium
-	{
-		"Exafunction/codeium.nvim",
-		enabled = false,
-		evant = { "InsertEnter" },
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("codeium").setup({})
-		end,
-	},
+      -- make autopairs and completion work together
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
 
-	{
-		"Exafunction/codeium.vim",
-		enabled = true,
-		event = "InsertEnter",
-		keys = {
-			{
-				"<C-g>",
-				function()
-					return vim.fn["codeium#Accept"]()
-				end,
-				mode = "i",
-				expr = true,
-				silent = true,
-			},
-			{
-				"<C-n>",
-				function()
-					return vim.fn["codeium#CycleCompletions"](1)
-				end,
-				mode = "i",
-				expr = true,
-				silent = true,
-			},
-			{
-				"<C-p>",
-				function()
-					return vim.fn["codeium#CycleCompletions"](-1)
-				end,
-				mode = "i",
-				expr = true,
-				silent = true,
-			},
-			{
-				"<C-x>",
-				function()
-					return vim.fn["codeium#Clear"]()
-				end,
-				mode = "i",
-				expr = true,
-				silent = true,
-			},
-		},
-	},
-
-	-- autopairs
-	{
-		"windwp/nvim-autopairs",
-		event = { "InsertEnter" },
-		dependencies = {
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			-- import nvim-autopairs
-			local autopairs = require("nvim-autopairs")
-
-			-- configure autopairs
-			autopairs.setup({
-				check_ts = true, -- enable treesitter
-				ts_config = {
-					lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-					javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-					java = false, -- don't check treesitter on java
-				},
-			})
-
-			-- import nvim-autopairs completion functionality
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-			-- import nvim-cmp plugin (completions plugin)
-			local cmp = require("cmp")
-
-			-- make autopairs and completion work together
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		end,
-	},
-
-	-- lspsaga nvim
-	-- {
-	-- 	"nvimdev/lspsaga.nvim",
-	-- 	config = function()
-	-- 		require("lspsaga").setup({})
-	-- 	end,
-	-- 	dependencies = {
-	-- 		"nvim-treesitter/nvim-treesitter", -- optional
-	-- 		"nvim-tree/nvim-web-devicons", -- optional
-	-- 	},
-	-- },
-	--
-
-	{
-		"glepnir/lspsaga.nvim",
-		lazy = false,
-		config = function()
-			require("lspsaga").setup({
-				-- keybinds for navigation in lspsaga window
-				move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-				-- use enter to open file with finder
-				finder_action_keys = {
-					open = "<CR>",
-				},
-				-- use enter to open file with definition preview
-				definition_action_keys = {
-					edit = "<CR>",
-				},
-			})
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-	},
+  -- lspsaga.nvim
+  {
+    "glepnir/lspsaga.nvim",
+    lazy = false,
+    config = function()
+      require("lspsaga").setup({
+        -- keybinds for navigation in lspsaga window
+        move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+        -- use enter to open file with finder
+        finder_action_keys = {
+          open = "<CR>",
+        },
+        -- use enter to open file with definition preview
+        definition_action_keys = {
+          edit = "<CR>",
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,190 +1,70 @@
+local Util = require("lazyvim.util")
 return {
-	-- dressing nvim
-	{
-		"stevearc/dressing.nvim",
-		lazy = true,
-		init = function()
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.ui.select = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.select(...)
-			end
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.ui.input = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.input(...)
-			end
-		end,
-	},
+  { import = "plugins.extras.ui.ui" },
 
-	-- lualine
-	{
-		"nvim-lualine/lualine.nvim",
-		lazy = false,
-		config = function()
-			require("lualine").setup({
-				options = {
-					-- colorscheme = "tokyonight",
-					colorscheme = "catppuccin",
-					globalstatus = true,
-					component_separators = { left = "", right = "" },
-					section_separators = { left = "", right = "" },
-					-- make lualine hidden when alpha is shown
-					disabled_filetypes = { "alpha", "dashboard" },
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = {
-						"buffers",
-					},
-					lualine_c = { "branch", "diff", "diagnostics" },
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = { "location" },
-				},
-			})
-		end,
-	},
+  -- bufferline disable
+  { "akinsho/bufferline.nvim", enabled = false },
 
-	-- inednt-blankline
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			indent = {
-				char = "│",
-				tab_char = "│",
-			},
-			scope = { enabled = true },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-			},
-		},
-	},
+  -- lualine
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   lazy = false,
+  --   config = function()
+  --     require("lualine").setup({
+  --       options = {
+  --         -- colorscheme = "tokyonight",
+  --         colorscheme = "catppuccin",
+  --         globalstatus = true,
+  --         component_separators = { left = "", right = "" },
+  --         section_separators = { left = "", right = "" },
+  --         -- make lualine hidden when alpha is shown
+  --         disabled_filetypes = { "alpha", "dashboard" },
+  --       },
+  --       sections = {
+  --         lualine_a = { "mode" },
+  --         lualine_b = {
+  --           "buffers",
+  --         },
+  --         lualine_c = { "branch", "diff", "diagnostics" },
+  --         lualine_x = {},
+  --         lualine_y = {},
+  --         lualine_z = { "location" },
+  --       },
+  --     })
+  --   end,
+  -- },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      return {
+        options = {
+          theme = "auto",
+          globalstatus = true,
+          disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "buffers" },
+          lualine_c = { "branch", "diff", "diagnostics" },
+          lualine_x = {
+            { "filetype", icon_only = true, separator = " ", padding = { left = 1, right = 1 } },
+          },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        table.insert(opts.sections.lualine_x, 2, require("lazyvim.util").lualine.cmp_source("codeium")),
+      }
+    end,
+  },
 
-	-- mini-indentscope
-	{
-		"echasnovski/mini.indentscope",
-		version = false,
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			symbol = "▏",
-			options = { try_as_border = true },
-		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
-	},
-
-	-- zen mode
-	{
-		"folke/zen-mode.nvim",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
-
-	-- noice
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			routes = {
-				{
-					view = "notify",
-					filter = { event = "msg_showmode" },
-				},
-			},
-			lsp = {
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true,
-				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = false,
-				lsp_doc_border = false,
-			},
-		},
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			{
-				"rcarriga/nvim-notify",
-				config = function()
-					require("notify").setup({
-						background_colour = "#000000",
-						stages = "fade",
-						render = "compact",
-						timeout = 1000,
-					})
-				end,
-				keys = {
-					{
-						"<leader>un",
-						"<cmd>lua require('notify').dismiss()<cr>",
-						desc = "Dismiss all notifications",
-					},
-				},
-			},
-		},
-	},
-
-	-- nvim-web-devicons
-	{
-		"nvim-tree/nvim-web-devicons",
-		lazy = true,
-	},
-
-	-- nui.nvim
-	{
-		"MunifTanjim/nui.nvim",
-		lazy = true,
-	},
-
-	-- alpha
-	{
-		"goolord/alpha-nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		lazy = false,
-
-		config = function()
-			require("alpha").setup(require("alpha.themes.dashboard").config)
-		end,
-	},
+  -- notify
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+      background_colour = "#000000",
+      stages = "fade",
+      render = "compact",
+    },
+  },
 }
