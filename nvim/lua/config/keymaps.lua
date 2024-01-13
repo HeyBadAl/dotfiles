0b13154d446f865008f9b54777ef3db7a6da8e37
Badal <badalWork1@gmail.com>
2024-01-13 12:16:40 +0530

    chore: remove same keybindings as docs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,57 +4,16 @@ local map = Util.safe_keymap_set
 
 -- Window management
 map("n", "<leader>sv", "<cmd>vsplit<cr>", { desc = "Split vertically" })
-map("n", "<leader>sh", "<cmd>split<cr>", { desc = "Split horizontally" })
+map("n", "<leader>ss", "<cmd>split<cr>", { desc = "Split horizontally" })
 
 -- show full file-path
 map("n", "<leader>pa", "<cmd>echo expand('%:p')<cr>", { desc = "Full file name" })
-
--- save file
-map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
-
---keywordprg
-map("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
 
 -- Directory Navigation
 map("n", "<leader>e", ":NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
 map("n", "<leader>m", ":NvimTreeFocus<cr>", { desc = "Focus NvimTree" })
 
--- lazygit
-map("n", "<leader>gg", function()
-  Util.terminal({ "lazygit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false })
-end, { desc = "Lazygit (root dir)" })
-map("n", "<leader>gG", function()
-  Util.terminal({ "lazygit" }, { esc_esc = false, ctrl_hjkl = false })
-end, { desc = "Lazygit (cwd)" })
-
--- quit
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
-
--- LazyVim Changelog
-map("n", "<leader>L", function()
-  Util.news.changelog()
-end, { desc = "LazyVim Changelog" })
-
--- floating terminal
-local lazyterm = function()
-  Util.terminal(nil, { cwd = Util.root() })
-end
-map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
-map("n", "<leader>fT", function()
-  Util.terminal()
-end, { desc = "Terminal (cwd)" })
--- map("n", "<leader>-/>", lazyterm, { desc = "Terminal (root dir)" })
-map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
-
--- windows
-map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
-map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
-map("n", "<leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
-map("n", "<leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
-map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
-map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
-
 -- comments
 local api = vim.api
-api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
-api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
+api.nvim_set_keymap("n", "<leader>cc", "gcc", { noremap = false })
+api.nvim_set_keymap("v", "<leader>cc", "gcc", { noremap = false })
