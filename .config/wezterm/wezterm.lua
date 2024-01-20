0c277d06f9160b4132c8a71db7295f80e3c4e13a
Badal <badalWork1@gmail.com>
2024-01-20 16:31:57 +0530

    chore: changes wallpaper

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -20,14 +20,11 @@ config.line_height = 1
 
 -- config.max_fps = 144
 config.color_scheme = "Catppuccin Mocha"
-
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
-
 config.enable_scroll_bar = false
-
 config.window_padding = {
 	left = 0,
 	right = 0,
@@ -38,18 +35,8 @@ config.window_padding = {
 -- config.window_background_opacity = 0.97
 local root_name = "/home/badal/"
 config.window_background_image = root_name .. "dotfiles/screenshots/dark_apple.jpg"
--- config.window_background_image = root_name .. "Desktop/2.jpg"
 config.window_background_image_hsb = {
-	-- Darken the background image by reducing it to 1/3rd
 	brightness = 0.008,
-
-	-- You can adjust the hue by scaling its value.
-	-- a multiplier of 1.0 leaves the value unchanged.
-	hue = 1,
-
-	-- You can adjust the saturation also.
-	saturation = 1,
 }
 
--- and finally, return the configuration to wezterm
 return config
