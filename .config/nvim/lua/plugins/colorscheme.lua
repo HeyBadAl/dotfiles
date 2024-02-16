8b30ff39aac5fe66d2d09f3462a5eec6f2abd5ac
Badal <badalWork1@gmail.com>
2024-02-16 10:19:23 +0530

    chore: updates mini.indentscope

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
     priority = 1000,
     config = function()
       require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        flavour = "mocha", -- latte, frappe, macchiato, mochacol
         transparent_background = true,
         show_end_of_buffer = false,
 
