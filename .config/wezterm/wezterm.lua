e98531fa0fdda5536eddf527fa51cf856fd8d6ee
Badal <badalWork1@gmail.com>
2024-03-15 19:37:38 +0530

    feat: added some more pictures

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,7 +18,9 @@ config.font = wezterm.font("JetBrainsMonoNL NF")
 config.font_size = 13
 
 config.max_fps = 145
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Sonokai (Gogh)"
+-- config.color_scheme = "Catppuccin Latte"
+config.color_scheme = "Tokyo Night"
 config.use_fancy_tab_bar = false
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
@@ -27,17 +29,18 @@ config.enable_scroll_bar = false
 config.window_padding = {
 	left = 0,
 	right = 0,
-	top = 20,
+	top = 22,
 	bottom = 0,
 }
 
--- config.window_background_opacity = 1.9
--- local root_name = "/home/badal/"
--- config.window_background_image = root_name .. ".dotfiles/screenshots/dark_apple.jpg"
--- config.window_background_image_hsb = {
--- 	brightness = 1.006,
--- }
+config.window_background_opacity = 0.78
+local root_name = "/home/badal/"
+config.window_background_image = root_name .. ".dotfiles/screenshots/car_2.jpg"
+config.window_background_image_hsb = {
+	brightness = 0.08,
+	-- saturation = 0.905,
+}
 
-config.text_background_opacity = 0.62
+config.text_background_opacity = 0.79
 
 return config
