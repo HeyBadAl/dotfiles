30f59127ee2f9cd0799315075251dd456ef6758c
Badal <badalWork1@gmail.com>
2024-06-09 13:17:42 +0530

    remove codeium

--- .config/nvim/lua/plugins/extras/coding/coding.lua
+++ .config/nvim/lua/plugins/extras/coding/coding.lua
@@ -1,82 +1,82 @@
 return {
   -- codeium
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
-      },
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "codeium",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = {
+  --     {
+  --       "Exafunction/codeium.nvim",
+  --       cmd = "Codeium",
+  --       build = ":Codeium Auth",
+  --       opts = {},
+  --     },
+  --   },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, 1, {
+  --       name = "codeium",
+  --       group_index = 1,
+  --       priority = 100,
+  --     })
+  --   end,
+  -- },
 
   -- codeium
-  {
-    "Exafunction/codeium.nvim",
-    enabled = false,
-    evant = { "InsertEnter" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      require("codeium").setup({})
-    end,
-  },
-
-  {
-    "Exafunction/codeium.vim",
-    enabled = true,
-    event = "InsertEnter",
-    keys = {
-      {
-        "<C-g>",
-        function()
-          return vim.fn["codeium#Accept"]()
-        end,
-        mode = "i",
-        expr = true,
-        silent = true,
-      },
-      {
-        "<C-n>",
-        function()
-          return vim.fn["codeium#CycleCompletions"](1)
-        end,
-        mode = "i",
-        expr = true,
-        silent = true,
-      },
-      {
-        "<C-p>",
-        function()
-          return vim.fn["codeium#CycleCompletions"](-1)
-        end,
-        mode = "i",
-        expr = true,
-        silent = true,
-      },
-      {
-        "<C-x>",
-        function()
-          return vim.fn["codeium#Clear"]()
-        end,
-        mode = "i",
-        expr = true,
-        silent = true,
-      },
-    },
-  },
+  -- {
+  --   "Exafunction/codeium.nvim",
+  --   enabled = false,
+  --   evant = { "InsertEnter" },
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "hrsh7th/nvim-cmp",
+  --   },
+  --   config = function()
+  --     require("codeium").setup({})
+  --   end,
+  -- },
+  --
+  -- {
+  --   "Exafunction/codeium.vim",
+  --   enabled = true,
+  --   event = "InsertEnter",
+  --   keys = {
+  --     {
+  --       "<C-g>",
+  --       function()
+  --         return vim.fn["codeium#Accept"]()
+  --       end,
+  --       mode = "i",
+  --       expr = true,
+  --       silent = true,
+  --     },
+  --     {
+  --       "<C-n>",
+  --       function()
+  --         return vim.fn["codeium#CycleCompletions"](1)
+  --       end,
+  --       mode = "i",
+  --       expr = true,
+  --       silent = true,
+  --     },
+  --     {
+  --       "<C-p>",
+  --       function()
+  --         return vim.fn["codeium#CycleCompletions"](-1)
+  --       end,
+  --       mode = "i",
+  --       expr = true,
+  --       silent = true,
+  --     },
+  --     {
+  --       "<C-x>",
+  --       function()
+  --         return vim.fn["codeium#Clear"]()
+  --       end,
+  --       mode = "i",
+  --       expr = true,
+  --       silent = true,
+  --     },
+  --   },
+  -- },
 
   -- lspkind.nvim
   {
